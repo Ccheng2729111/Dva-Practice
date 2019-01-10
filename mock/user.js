78264734e6714f15c31fdd8c74ff4924c265e39c
ChengJian <Bkcheng2729111@163.com>
2019-01-10 16:31:29 +0800

    修复删除报错。

--- mock/user.js
+++ mock/user.js
@@ -6,22 +6,24 @@ let db=Mock.mock({
     Email:'@email',
     Website:Mock.Random.url(),
     birthday: '@date("yyyy-MM-dd")',  // 日期
-    city: '@city(true)',   // 中国城市
+    city: '@city(true)',   // 中国城市111
   }]
 });
 
 module.exports={
   [`GET /api/users`](req,res){
-
+    // console.log(req);
+    // console.log("-------req---------------");
+    // console.log(res);
+    // console.log("-------res---------------");
     res.status(200).json(db);
   },
 
-  [`DELETE /api/users`](req,res){
-
-    let user=req.body;
-    console.log(req);
-    user.id=Mock.mock('@id');
-    db.data.filter((i)=>i.id===user.id);
+  [`DELETE /api/users/:id`](req,res){
+    //req.params去接受DELETE接口传回的id进行模拟删除操作。
+    let user=req.params;
+    console.log(req.params);
+    db.list = db.list.filter((i)=>i.id!==user.id);
 
     res.status(200).json(user);
   }
